1b079edd0a2d6f61985f4ab6cfd6a3a086d683b3
Soul <hansol1501@gmail.com>
2024-07-27 16:42:10 +0900

    [Silver IV] Title: 좋은 단어, Time: 144 ms, Memory: 31552 KB -BaekjoonHub

---
+++ 백준/Silver/3986. 좋은 단어/좋은 단어.py
@@ -0,0 +1,22 @@
+N = int(input())
+
+good_word_count = 0
+
+for i in range(N):
+    word = input()
+    stack = []
+    
+    for character in word:
+        if len(stack) == 0:
+            stack.append(character)
+            continue
+        
+        if stack[-1] == character:
+            stack.pop(-1)
+            continue
+        
+        stack.append(character)
+    
+    good_word_count += 1 if len(stack) == 0 else 0
+    
+print(good_word_count)
