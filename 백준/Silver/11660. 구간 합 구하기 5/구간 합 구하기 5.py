3d1d854a59dbe417042b33d5a28992816196cfe8
Soul <hansol1501@gmail.com>
2024-08-30 15:39:55 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 3592 ms, Memory: 94508 KB -BaekjoonHub

---
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.py
@@ -0,0 +1,24 @@
+import copy
+n, m = [int(x) for x in input().split()]
+nums = [[int(x)for x in input().split()] for _ in range(n)]
+coordinates = [[int(x)for x in input().split()] for _ in range(m)]
+
+for i in range(n):
+    for j in range(n):
+        left = nums[i][j-1] if j > 0 else 0
+        top = nums[i-1][j] if i > 0 else 0
+        cross = nums[i-1][j-1] if i > 0 and j > 0 else 0
+        nums[i][j] += left + top - cross
+        
+
+def get_partial_sum(list: list[int]) -> int:
+    x1, y1, x2, y2 = list
+    raw = nums[x2-1][y2-1]
+    left = nums[x2-1][y1-2] if y1-2 > -1 else 0
+    top = nums[x1-2][y2-1] if x1-2 > -1 else 0
+    cross = nums[x1-2][y1-2] if y1-2 > -1 and x1-2 > -1 else 0
+    return nums[x2-1][y2-1] - left - top + cross
+    
+    
+for coordinate in coordinates:
+    print(get_partial_sum(coordinate))
